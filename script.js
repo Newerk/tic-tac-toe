e8a7abbbebf2980ce41ff0651b34972f4822c7eb
newerk <juniusowens@yahoo.com>
2022-08-16 11:17:39 -0400

    Add Basic onClick Functionality to Board
    Player can currently Insert X where ever they click on the board

--- script.js
+++ script.js
@@ -5,14 +5,41 @@ While the minimum number of moves to win a game is five, the maximum number of m
 
 //Factory funtions return objects. Modules are the same, except they immediately invoke the function by putting "();"" at the end
 
+/*When a space in the gameboard is clicked it will get the symbol of the current player and output it onto the board.
+A player cannot place their symbol more than once per turn. */
+
 //gameboard Module
 const game = (() => {
 
+
+
+
     const Gameboard = {
         gameboard:
-               ["", "", "",
-                "", "", "",
-                "", "", ""]
+            [null, null, null,
+                null, null, null,
+                null, null, null],
+
+
+        buildBoard: function () {
+            let count = 0
+            this.gameboard.forEach(el => {
+                el = 0;
+                const square = document.getElementById(`square_${count}`);
+                
+                if (count !== 9) {
+                    const insertSymbol = () => {
+                        const playerChoice = PlayerInfo('X');
+                        square.textContent = playerChoice.getSymbol();
+                    }
+                    square.addEventListener('click', insertSymbol)
+                    console.log(el);
+                    count++;
+                }
+
+            })
+
+        }
     }
 
     //controls flow of game. perhaps the winning/tie conditions are placed here
@@ -26,15 +53,17 @@ const game = (() => {
     }
 })();
 
-const PlayerInfo = (symbol) => {
-    const getSymbol = () => console.log(`my symbol is ${symbol}`);
+game.Gameboard.buildBoard();
 
-    return {getSymbol}
+const PlayerInfo = (symbol) => {
+    const getSymbol = () => symbol;
+
+    return { getSymbol }
 }
 
-const choseX = PlayerInfo('X');
-choseX.getSymbol();
+// const choseX = PlayerInfo('X');
+// choseX.getSymbol();
 
 
 
-console.log(game.Gameboard.gameboard);
+// console.log(game.Gameboard.gameboard);
