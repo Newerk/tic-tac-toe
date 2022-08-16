ec01c02daa3501a776caaf17a67d4d82b9fed801
newerk <juniusowens@yahoo.com>
2022-08-16 12:16:37 -0400

    Add Comments To Help Plan For this Project

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ A player cannot place their symbol more than once per turn. */
 
 /*The symbols should appear on the board based on the index in the game.Gameboard.gameboard index */
 
+/*tic tac toe win conditions (can win within 5 to 8 rounds) if game reaches 9 rounds, its a tie
+if player symbols are index's:
+ 0,1,2 or 3,4,5 or 6,7,8 (horizonal wins)
+ 0,3,6 or 1,4,7 or 2,5,8 (vertical wins)
+ 0,4,8 or 2,4,6 (diagnol wins)
+
+ THERE ARE A TOTAL OF 8 WIN CONDITIONS
+
+ Everytime a player or the computer(will worry about this later) inserts their symbol on the board, it will check for the win conditions
+*/
+
+
 //gameboard Module
 const game = (() => {
 
@@ -27,7 +39,6 @@ const game = (() => {
 
                 if (count !== 9) {
                     square.textContent = Gameboard.gameboard[count];
-
                     count++;
                 }
 
